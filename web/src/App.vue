fb4e9374de4c2bcdf39344a134030b9f093fd63b
alan <alan.wei@live.com>
2020-04-26 18:20:29 +0800

    feat: 支持配置

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
+<script>
+import { getJSON, ShareStore } from "./lib";
+export default {
+  data() {
+    return {
+      share: ShareStore
+    };
+  },
+  mounted() {
+    getJSON("/resources/config.json").then(config => {
+      this.share.config = config;
+      document.title = this.share.config.title;
+    });
+  }
+};
+</script>
---
+++ web/src/lib/store.js
@@ -0,0 +1,6 @@
+export const ShareStore = {
+    config: {
+        title: undefined,
+        copyBase: undefined
+    }
+};
