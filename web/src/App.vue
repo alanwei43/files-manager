23d26f98914b29c7bb7792f774cacec7f25af1c2
alan <alan.wei@live.com>
2020-04-26 11:13:58 +0800

    init

---
+++ web/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-fluid">
+    <div class="col-sm-12">
+      <list-files />
+    </div>
+  </div>
+</template>
+
+<script>
+import ListFiles from "./components/ListFiles.vue";
+
+export default {
+  name: "App",
+  components: {
+    "list-files": ListFiles
+  }
+};
+</script>
+
